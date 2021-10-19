c1d4b08d03dae0c58526d4c5d6211f0c21f86d26
Fredrik Haglund <fredrik@petitcircuitlab.com>
2021-10-19 16:55:32 +0200

    🚀 init commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# fr3h4g.eu
+
+This is my personal webpage for https://fr3h4g.eu
+
+### Develop
+
+`yarn install`
+`yarn develop`
+
+### Build
+
+`yarn install`
+`yarn build`
---
+++ src/pages/Index.vue
@@ -0,0 +1,33 @@
+<template>
+  <Layout>
+    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
+    <h1>Hello! 👋🏼</h1>
+
+    <p>
+      My name is Fredrik.
+    </p>
+
+    <p class="home-links">
+      <a href="https://facebook.com/fr3h4g" target="_blank" rel="noopener"
+        >Facebook</a
+      >
+      <a href="https://github.com/fr3h4g" target="_blank" rel="noopener"
+        >GitHub</a
+      >
+    </p>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: "Hello! 👋🏼",
+  },
+};
+</script>
+
+<style>
+.home-links a {
+  margin-right: 1rem;
+}
+</style>
